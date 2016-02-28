b12fc830ddf84516b55050453a52bc26b5c5c08d
Sam Stonehouse <sgstonehouse@googlemail.com>
2016-02-28 15:35:35 +0000

    Bugfix, now promises are actually resolved

--- main.js
+++ main.js
@@ -9,34 +9,38 @@ function importDatabase(jsonData, Set, Card) {
 		}
 
 		// Remove the old sets
-		console.log('Removing old sets');
+		console.log('Removing old cards and sets');
 		Set.remove({}, function(err) {
-			if (err) { throw err; }
-			console.log('Sucessfully removed old sets, re-adding');
-			async.eachSeries(setList, function(set, setCallback) {
-				// Remove cards from the set
-				var cards = set.cards;
-				delete set.cards;
+			if (err) { return reject(err); }
+			Card.remove({}, function(err) {
+				if (err) { return reject(err); }
+				async.eachSeries(setList, function(set, setCallback) {
+					// Remove cards from the set
+					var cards = set.cards;
+					delete set.cards;
 
-				// Create and save new set
-				var newSet = new Set(set);
-				newSet.save(function(err) {
-					if (err) { throw err; }
-					console.log('Saved set: ' + set.name + ', saving cards...');
+					// Create and save new set
+					var newSet = new Set(set);
+					newSet.save(function(err) {
+						if (err) { return reject(err); }
+						console.log('Saved set: ' + set.name + ', saving cards...');
 
-					async.each(cards, function(card, cardCallback) {
-						card.setName = set.name;
-						card.setCode = set.code;
-						// Create and save each individual card
-						var newCard = new Card(card);
-						newCard.save(function(err) {
-							if (err) { throw err; }
-							cardCallback();
-						});
-					}, setCallback);
+						async.each(cards, function(card, cardCallback) {
+							// Add set code and name to each card before saving
+							card.setCode = set.code;
+							card.setName = set.name;
+							// Create and save each individual card
+							var newCard = new Card(card);
+							newCard.save(function(err) {
+								if (err) { return reject(err); }
+								cardCallback();
+							});
+						}, setCallback);
+					});
+				}, function() {
+					console.log('Import complete');
+					resolve();
 				});
-			}, function() {
-				console.log('Import complete');
 			});
 		});
 	});
